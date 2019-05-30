00133eff8c63d238fbf0fec437a63a7433649841
huaxk <huaxk@163.com>
2019-05-30 16:44:04 +0800

    Implementation toEWKB

--- src/ogr/geometry.jl
+++ src/ogr/geometry.jl
@@ -185,6 +185,54 @@ function toWKB(geom::AbstractGeometry, order::OGRwkbByteOrder=GDAL.wkbNDR)
 end
 
 """
+Convert a geometry extended well known binary format.
+
+### Parameters
+* `geom`: handle on the geometry to convert to a well know binary data from.
+* `order`: One of wkbXDR or [wkbNDR] indicating MSB or LSB byte order resp.
+"""
+function toEWKB(geom::AbstractGeometry, order::OGRwkbByteOrder=GDAL.wkbNDR)
+    buffer = toWKB(geom, order)
+    ref = getspatialref(geom)
+    srid = getsrid(ref)
+    if srid != nothing
+        sridbytes = srid2bytes(srid)
+        buffer = vcat(setsridflag!(buffer[1:5]), sridbytes, buffer[6:end])
+    end
+    buffer
+end
+
+function getsrid(ref::AbstractSpatialRef)
+    srid = nothing
+    if ref.ptr != C_NULL
+        epsgstr = getattrvalue(ref, "AUTHORITY", 0)
+        sridstr = getattrvalue(ref, "AUTHORITY", 1)
+        if epsgstr != nothing && sridstr != nothing && uppercase(epsgstr) == "EPSG"
+            srid = parse(UInt32, sridstr)
+        end
+    end
+    srid
+end
+
+function srid2bytes(srid::UInt32)
+    io = IOBuffer()
+    Base.write(io, srid)
+    take!(io)
+end
+
+function setsridflag!(bytes::Vector{UInt8})
+    length(bytes) < 5 && @error "wkb format error"
+    if bytes[1] == 0x01
+        bytes[5] = 0x20
+    elseif bytes[1] == 0x00
+        bytes[2] == 0x20
+    else
+        @error "endian format error"
+    end
+    bytes
+end
+
+"""
 Convert a geometry into SFSQL 1.2 / ISO SQL/MM Part 3 well known binary format.
 
 ### Parameters
